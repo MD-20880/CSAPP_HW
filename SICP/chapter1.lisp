0c32dbb149e507da55f423a5501526ba64efe961
dengmingzhang <mingzhang.deng-e0142@aqara.com>
2024-07-12 15:37:54 +0800

    add answer to SICP 1.7 1.8

--- SICP/chapter1.lisp
+++ SICP/chapter1.lisp
@@ -11,3 +11,31 @@
                 ((decre b c a) (+ b c)) 
                 ((decre c a b) (+ c a)) 
                 ((decre c b a) (+ c b))))
+
+1.7 
+(define (improve y x) (/ (+ y (/ x y)) 2))
+(define (good-enough dy y) (< (/ dy y) 0.01))
+(define (abs x) ((if (> 0 x) - +) x ))
+(define (sqrt-iter guess delta x) (if (good-enough delta x) guess (sqrt-iter (improve guess x) (abs (- (improve guess x) guess)) x)))
+(sqrt-iter 1 1 2)
+
+1.8
+(define (improve y x) (/ (+ (/ x (* y y) (* 2 y))) 3))
+(define (good-enough dy y) (< (/ dy y) 0.5))
+(define (abs x) ((if (> 0 x) - +) x ))
+(define (cube-sqrt-iter guess delta x) (if (good-enough delta x) guess (cube-sqrt-iter (improve guess x) (abs (- (improve guess x) guess)) x)))
+(cube-sqrt-iter 2.5 1 27)
+
+1.11
+(define (f-iter n3 n2 n1 nc n) (cond
+                                ((< n 3) n)
+                                ((< nc n) (f-iter n2 n1 (f-next n3 n2 n1) (+ nc 1) n))
+                                (else n1)
+                                ))
+(define (f n) (f-iter 0 1 2 2 n))
+(define (f-next n3 n2 n1) (+ (* n3 3) (* n2 2) n1))
+
+
+1.12
+
+1.13
